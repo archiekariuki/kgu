753fbd4ff91d440acaca4e8efee237d50772ab7f
Archie Kariuki <kariuka@UNNAIROBI.UN.ORG>
2020-05-22 06:34:54 +0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+    <navbar />
+    
+  </div>
+</template>
+
+<script>
+import navbar from './navbar'
+export default {
+  name: 'app',
+  components:{
+    navbar
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+
+<nav>
+
+    <ul>
+        <li>Home</li>
+        <li>Tournamens</li>
+
+    </ul>
+
+</nav>
+</template>
+
+<script>
+export default {
+    name:'navbar',
+data (){
+    return{
+
+    }
+}
+}
+</script>
+
+<style>
+nav{
+    text-align: center;
+}
+nav ul{
+padding: 0;
+}
+nav li{
+    font-weight: 10;
+    text-decoration: underline;
+    display: inline;
+}
+</style>
